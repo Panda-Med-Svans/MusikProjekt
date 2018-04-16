8eb85deb2db536c57b44474b8e2017c6315b4286
AlexTallen <AlexTallen@PMS>
2018-04-16 17:38:39 +0200

    Added new Project
    With folders and multiple scenes to work in

---
+++ Assets/Scripts/Movement/CharacterJump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterJump : MonoBehaviour {
+
+    public int numberOfDoubleJumps = 1;
+    private int doubleJumpCount = 0;
+    public float jumpSpeed = 5f;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        doubleJumpCount = numberOfDoubleJumps;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(doubleJumpCount);
+        if (Input.GetKeyDown(KeyCode.Space) && doubleJumpCount <= numberOfDoubleJumps)
+        {
+            rb.velocity += jumpSpeed * Vector3.up;
+            doubleJumpCount++;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            doubleJumpCount = 0;
+        }
+        if (collision.gameObject.tag == "Bad")
+        {
+            Bad();
+        }
+    }
+    void Bad()
+    {
+        //TODO something when you touch bad
+    }
+}
---
+++ Assets/Scripts/Movement/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    //TODO: Automatisk rörelse
+    //TODO: Hopp
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
