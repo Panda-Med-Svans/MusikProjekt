76294a708b680117125328cb2eed145d6395c673
NoManLukas <38425641+NoManLukas@users.noreply.github.com>
2018-04-16 17:51:10 +0200

    AutoRun update

--- Assets/Scripts/Movement/CharacterMovement.cs
+++ Assets/Scripts/Movement/CharacterMovement.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour {
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
 
-    //TODO: Automatisk rörelse
-    //TODO: Hopp
-
+    private Vector2 moveDirection = Vector2.zero;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController player = GetComponent<CharacterController>();
+        if (player.isGrounded)
+        {
+            moveDirection.y = 0;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        else
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        moveDirection.x = speed;
+
+        player.Move(moveDirection * Time.deltaTime);
+    }
 }
