553dc552c3ea7e90c6e2065f6f5aa64f7b56ac0f
AlexTallen <AlexTallen@PMS>
2018-04-16 18:39:09 +0200

    Update
    Some Materials
    Some Mechanics
    Some Folders

---
+++ Assets/Scripts/Other/AutoRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+
+    public float rotationX;
+    public float rotationY;
+    public float rotationZ;
+    private float rotationSpeed = 90f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(rotationX, rotationY, rotationZ) * rotationSpeed * Time.deltaTime);
+    }
+}
